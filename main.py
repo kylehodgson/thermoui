af59e6c49efb9f013059b1576538c68f64923e60
Kyle Hodgson <kyle.hodgson@gmail.com>
2022-12-04 13:14:19 -0500

    updated library, actually working

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from PIL import Image,ImageDraw,ImageFont
-from thirdparty.TP_lib import epd2in13_V3
-from thirdparty.TP_lib import gt1151
+from thirdparty.waveshare_epd import epd2in13b_V4
 import time
 
-epd=epd2in13_V3.EPD()
-gt = gt1151.GT1151()
+epd=epd2in13b_V4.EPD()
 
 try:
-    epd.init(epd.FULL_UPDATE)
-    gt.GT_Init()
-    epd.Clear(0xFF)
-
-    image=Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(image)
-    font=ImageFont.truetype("bmp/Font.ttc", 24)
-    draw.text((8, 12), 'thermoWorld', font = font, fill = 155)
-    epd.display(epd.getbuffer(image))
+    epd.init()
+    font20=ImageFont.truetype("bmp/Font.ttc", 20)
+    font18=ImageFont.truetype("bmp/Font.ttc", 18)
+    image_bw=Image.new('1', (epd.height, epd.width), 255)
+    image_ry=Image.new('1', (epd.height, epd.width), 255)
+    draw_bw = ImageDraw.Draw(image_bw)
+    draw_ry = ImageDraw.Draw(image_ry)
+    draw_bw=ImageDraw.Draw(image_bw)
+    draw_ry=ImageDraw.Draw(image_ry)
+    draw_bw.text((8, 8), 'thermoWorld', font = font20, fill = 0)
+    draw_bw.text((8, 32), 'thermo UI', font = font18, fill = 0)
+    draw_ry.line((165,50,165,100), fill = 0)
+    epd.display(epd.getbuffer(image_bw),epd.getbuffer(image_ry))
     time.sleep(2)
+    epd.sleep()
 except Exception as e:
-    print(f"caught {typeof(e)} exception {e}")
+    print(f"caught {repr(e)} exception {e}")
 
 
-print(f"exiting module")
-epd.Dev_exit()
 exit()
 
